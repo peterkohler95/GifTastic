51480a6cc9782cc72b673bd359b3597a90472b4b
Peter K <peterkohler95@gmail.com>
2018-08-15 10:31:02 -0500

    connected to giphy API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,92 +1,95 @@
 // initial array of actors
 var actorArray = ["Adam Sandler", "Will Ferrell", "Steve Carell", "Jim Carrey", "Kristen Wiig", "Tina Fey"];
 
-// Convert actor array into array of buttons
+// displayActorInfo function re-renders HTML to display appropriate content
+function displayActorInfo() {
+
+    var actor = $(this).attr("data-person");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        actor + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+            personImage.addClass("gif")
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+
+            $("#gifs-appear-here").prepend(gifDiv);
+        }
+
+    });
+};
 
 
-actorArray.forEach(function (element) {
-    button = $("<button>");
+// Function for displaying movie data
+function renderButtons() {
 
-    button.append(element);
-    button.attr("data-id");
-    button.addClass("actorBtnClass");
+    // Deleting the movies prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $(".actorBtns").empty();
 
+    // Looping through the array of movies
+    for (var i = 0; i < actorArray.length; i++) {
 
-    $(".actorBtns").append(button);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // Then dynamicaly generating buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class of movie-btn to our button
+        a.addClass("actor-btn");
+        // Adding a data-attribute
+        a.attr("data-person", actorArray[i]);
+        // Providing the initial button text
+        a.text(actorArray[i]);
+        // Adding the button to the buttons-view div
+        $(".actorBtns").append(a);
 
 
+    }
+}
 
+// This function handles events where a movie button is clicked
+$("#add-actor").on("click", function (event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var actor = $("#actor-input").val().trim();
 
+    // Adding movie from the textbox to our array
+    actorArray.push(actor);
 
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
 
+// Adding a click event listener to all elements with a class of "movie-btn"
+$(document).on("click", ".actor-btn", displayActorInfo);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
 
 
 // ==========================================================================
 
-
 $(".gif").on("click", function () {
-    // STEP ONE: study the html above.
-    // Look at all the data attributes.
-    // Run the file in the browser. Look at the images.
-
-    // After we complete steps 1 and 2 we'll be able to pause gifs from giphy.
-
-    // STEP TWO: make a variable named state and then store the image's data-state into it.
-    // Use the .attr() method for this.
-
-    // ============== FILL IN CODE HERE FOR STEP TWO =========================
-
-    // CODE GOES HERE
-
     var state = $(this).attr("data-state");
 
-
-    // =============================================
-
-    // STEP THREE: Check if the variable state is equal to 'still',
-    // then update the src attribute of this image to it's data-animate value,
-    // and update the data-state attribute to 'animate'.
-
-    // If state is equal to 'animate', then update the src attribute of this
-    // image to it's data-still value and update the data-state attribute to 'still'
-    // ============== FILL IN CODE HERE FOR STEP THREE =========================
-
-    // CODE GOES HERE
     var stillImageSrc = $(this).attr("data-still");
     var animateImageSrc = $(this).attr("data-animate");
 
-    // ==============================================
     if (state === "still") {
         $(this).attr("data-state", "animate");
         $(this).attr("src", animateImageSrc)
@@ -94,9 +97,4 @@ $(".gif").on("click", function () {
         $(this).attr("data-state", "still");
         $(this).attr("src", stillImageSrc);
     }
-
-    // STEP FOUR: open the file in the browser and click on the images.
-    // Then click again to pause.
-
-
 });
